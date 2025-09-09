fa8bfbe6db8a0bd14f611ae642c54c7a14b0321f
Mehdi Shokrizadeh <mahdi.shokri@gmail.com>
2025-09-09 19:22:11 +0330

    fix: remove extra sql files

--- extra/crypto_real_time.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Crypto_RealTime_Updates (
-    update_id INT PRIMARY KEY IDENTITY(1,1),  -- Auto-incrementing primary key for each real-time update
-    crypto_id INT FOREIGN KEY REFERENCES Cryptocurrency(crypto_id), -- Foreign key to the Cryptocurrency table
-    price FLOAT NOT NULL,                     -- Real-time last price of the cryptocurrency
-    change FLOAT,                             -- Price change from the previous update
-    changes_percentage FLOAT,                 -- Percentage change in price
-    timestamp DATETIME DEFAULT GETDATE()      -- Timestamp of the real-time update
-);
